ce407f4274efc37beb6c7c9f582e6892f8351da7
AriSilva94 <ariovaldo.bsjunior@gmail.com>
2025-07-22 13:08:47 -0300

    fix:auth login

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -127,14 +127,14 @@ export function AuthForm({ mode }: AuthFormProps) {
 
       {mode === "login" && (
         <>
-          <div className="relative my-6">
+          {/* <div className="relative my-6">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300"></div>
             </div>
             <div className="relative flex justify-center text-sm">
               <span className="px-2 bg-white text-gray-500">Ou</span>
             </div>
-          </div>
+          </div> */}
 
           <Button
             onClick={handleGoogleSignIn}
