44262980321b5bdac049a91ec72ff7e8b036fbc3
AriSilva94 <ariovaldo.bsjunior@gmail.com>
2025-08-10 00:29:11 -0300

    feat: add character in register flow

--- components/cadastro/CadastroHeader.tsx
+++ components/cadastro/CadastroHeader.tsx
@@ -1,12 +1,22 @@
 import { Typography } from "@/components/ui/Typography";
+import { CharacterSelector } from "./CharacterSelector";
 
 export function CadastroHeader() {
   return (
     <div className="mb-8">
-      <Typography variant="h1">Novo Registro</Typography>
-      <Typography variant="lead" className="mt-2">
-        Crie um novo registro usando JSON ou formato de texto
-      </Typography>
+      <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4">
+        <div>
+          <Typography variant="h1">Novo Registro</Typography>
+          <Typography variant="lead" className="mt-2">
+            Crie um novo registro usando JSON ou formato de texto
+          </Typography>
+        </div>
+        
+        {/* Seletor de personagem */}
+        <div className="lg:min-w-[300px]">
+          <CharacterSelector />
+        </div>
+      </div>
     </div>
   );
 }
